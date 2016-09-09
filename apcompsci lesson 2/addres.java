6fc9daf9c775cd846db5d1df3606bf1b3f881e33
andrewsbarrington <abarrington98@gmail.com>
2016-09-09 10:54:26 -0700

    lesson 2

---
+++ apcompsci lesson 2/addres.java
@@ -0,0 +1,14 @@
+public class addres
+{
+	public static void main(String[]args)
+	{
+		String first = "420 blazeit LN";
+		String second = " SAN DIEGO";
+		String third = " 91169 ";
+		
+		
+		System.out.println(first + second + third);
+	}
+	
+	
+}
---
+++ apcompsci lesson 2/lesson2.java
@@ -0,0 +1,21 @@
+public class lesson2
+{
+	public static void main(String[]args)
+	{
+		int l = 8;
+		int w = 6;
+		int h = 7;
+		
+		int cubeArea = 2*w*l + 2*h*l + 2*h*w;
+		
+		System.out.println("surface area of the cube"+" " + cubeArea);
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
