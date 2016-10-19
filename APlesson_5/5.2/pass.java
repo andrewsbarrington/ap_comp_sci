c7d9abd91d4211d364dc724aa4587733f5bb227c
andrewsbarrington <abarrington98@gmail.com>
2016-10-19 13:25:51 -0700

    lab 5

---
+++ APlesson_5/5.2/pass.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class pass
+{
+static Scanner kb;
+
+
+public static void main(String[]args)
+{
+	kb = new Scanner(System.in);
+	String username = "andrew";
+	String password = "boi";
+	pass();
+	
+}
+
+public static void pass()
+{
+	System.out.println("please enter your user name");
+	String useren = kb.next();
+	System.out.println("please enter your password");
+	String pasen = kb.next();
+	
+	if (useren.equals("andrew")&&pasen.equals("boi"))
+	{
+			System.out.println("access granted");
+		
+	}
+	else 
+	{
+		if(useren.equals("andrew"))
+		{
+		System.out.println("your password is incorrect");
+		}
+		else if (pasen.equals("boi"))
+		{
+			System.out.println("your username is incorrect");
+		}
+		else{
+			System.out.println("username and password are incorrect");
+		}
+	}
+	
+	
+}
+}
